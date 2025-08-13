6d566c11b216784ca63a0f564d56010d17f3c8fb
idrice junior <dongmojunior18@gmail.com>
2025-08-12 21:22:34 -0500

    all done

--- personal/models.py
+++ personal/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    first_name = models.CharField(max_length=120)
+    age = models.IntegerField(default=0)
+    sex = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.first_name}"
+    
+class Product(models.Model):
+    creator = models.ForeignKey(User,on_delete=models.CASCADE)
+    name = models.CharField(max_length=120)
+    postdate = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}"
--- personal/views.py
+++ personal/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.conf import settings
+from .models import User,Product
 # Create your views here.
 
 def index(request):
+    user = User.objects.all()
     return render(request,'website/index.html',{
-        'MEDIA_URL':settings.MEDIA_URL
+        'MEDIA_URL':settings.MEDIA_URL,
+        'users':user
     })
