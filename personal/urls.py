1d98392ed6acdec7e1687f3c982fc4856064d44e
idrice junior <dongmojunior18@gmail.com>
2025-08-14 10:33:31 -0500

    bot

--- personal/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .import views
-from django.conf.urls.static import static
-from portfolio import settings
-
-urlpatterns = [
-    path('',views.index, name='index')
-
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
