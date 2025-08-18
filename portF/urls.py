b2c4c3dded3d2870876f473cdd62a26bee4bf613
idrice junior <dongmojunior18@gmail.com>
2025-08-17 21:16:22 -0500

    image

--- portF/urls.py
+++ portF/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from port import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index')
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
